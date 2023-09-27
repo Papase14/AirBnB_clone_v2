c2891d49776eb05d5890dc135175309a32b8c082
Tshupane Morake <tshupane@outlook.com>
2023-09-28 00:43:08 +0200

    8. List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,14 +12,26 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """display a HTML page with the states listed in alphabetical order"""
+    """
+    Display a HTML page with a list of states in alphabetical order.
+
+    Returns:
+        str: Rendered HTML template with the states variable.
+    """
     states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """closes the storage on teardown"""
+    """
+    Closes the storage connection after each request.
+
+    :param exception: An optional parameter that represents any exception that occurred during the request.
+    :type exception: Exception
+
+    :return: None
+    """
     storage.close()
 
 
