e82fa2d8603791f2302cdc4429c462e01fba20ce
Tshupane Morake <tshupane@outlook.com>
2023-09-27 22:50:19 +0200

    Docstring

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -10,16 +10,36 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
+    """
+    Returns a greeting message.
+
+    Returns:
+        str: A string containing the greeting message "Hello HBNB!".
+    """
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """
+    Returns the string "HBNB" when a request is made to the "/hbnb" URL.
+
+    :return: The string "HBNB"
+    """
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
+    """
+    Returns a string that starts with "C " followed by the value of `text` with underscores replaced by spaces.
+
+    Args:
+        text (str): The value of `text` is extracted from the URL.
+
+    Returns:
+        str: A string that starts with "C " followed by the value of `text` with underscores replaced by spaces.
+    """
     return "C " + text.replace("_", " ")
 
 
